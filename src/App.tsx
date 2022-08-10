ce85a3d43c2a411b9820d51fd02720426beda674
htutaungwai <defo1998@gmail.com>
2022-08-10 12:55:51 +0630

    added crud functions

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import InputField from "./components/InputField";
 import { Todo } from "./model";
+import TodoList from "./components/TodoList";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
@@ -10,18 +11,17 @@ const App: React.FC = () => {
 
   const handleAdd = (e: React.FormEvent) => {
     e.preventDefault();
-
     if (todo) {
       setTodos([...todos, { id: Date.now(), todo: todo, isDone: false }]);
       setTodo("");
     }
   };
 
-  console.log(todos);
   return (
     <div className="App">
       <span className="heading">Mingalar Task</span>
       <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <TodoList todos={todos} setTodos={setTodos} />
     </div>
   );
 };
